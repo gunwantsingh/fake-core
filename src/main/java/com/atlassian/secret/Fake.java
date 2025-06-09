3b08bf552763c2e30e7cc7072053ed8cbcb0b030
gunwantsingh <gunwant.s@gmail.com>
2025-06-09 21:08:27 +0530

    Update Fake.java

--- src/main/java/com/atlassian/secret/Fake.java
+++ src/main/java/com/atlassian/secret/Fake.java
@@ -2,7 +2,10 @@ package com.atlassian.secret;
 
 public class Fake {
     public static String message() {
+        String token = "Th15isaSecretToken";
+        String password = "This is a Password";
         return "This is a fake dependency!";
+        
     }
 }
 
